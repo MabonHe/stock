07d0fa2fe14a905849ee3f4f48f91e368624a2ee
hemaobin <hemaobinz@163.com>
2018-07-01 16:34:25 +0800

    update for get stock data from sina
    Signed-off-by: hemaobin <hemaobinz@163.com>

--- getdata.py
+++ getdata.py
@@ -12,5 +12,19 @@ import re
 import io
 sys.path.append('/home/hemaobin/workspace/web')
 import mysqldb
-str='http://hq.sinajs.cn/list=sz000651'
+url='http://hq.sinajs.cn/list=sz000651'
 
+headers = {'User-Agent':'gsi'}
+while True:
+    time.sleep(2)
+    request=urllib.request.Request(url, headers = headers)
+    try:
+        response=urllib.request.urlopen(request)
+        html = response.read()
+    except urllib.error.URLError as e:
+        print('urlerror!!')
+        time.sleep(12)
+    #print(html)
+    htmlstr = html.decode('GBK')
+    search=re.search(r'=(.*)',htmlstr)
+    print(search.group(1))
