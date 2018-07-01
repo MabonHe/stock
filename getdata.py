484028c93de44d306ee3de0b955952b10c2bbea3
hemaobin <hemaobinz@163.com>
2018-07-01 15:53:03 +0800

    add new file for get stock data from sina
    Signed-off-by: hemaobin <hemaobinz@163.com>

---
+++ getdata.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# coding=utf-8
+import pymysql
+import urllib.request
+import lxml.html
+import json
+import string
+import sys
+import time
+import random
+import re
+import io
+sys.path.append('/home/hemaobin/workspace/web')
+import mysqldb
+str='http://hq.sinajs.cn/list=sz000651'
