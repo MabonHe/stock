b6c053f2846323a1c96b24a5f638a9705141e78a
hemaobin <hemaobinz@163.com>
2018-07-08 21:09:46 +0800

    update

--- calculate.py
+++ calculate.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
+import pymysql
+import urllib.request
+import lxml.html
+import json
+import string
+import sys
+import time
+import random
+import re
+import io
+sys.path.append('/home/hemaobin/workspace/stock')
+import mysqldb
+symbollist = ['sz000651','sz000333','sz300104','sz300415','sh601777','sz300051']
+code_list = ['000651','000333','300104','300415','601777','300051']
