a168a7412f0f8fa54416e9b6552ac4cc05a68405
hemaobin <hemaobinz@163.com>
2018-07-09 23:37:12 +0800

    update

--- analyzer.py
+++ analyzer.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 # coding=utf-8
+import pymysql
+import urllib.request
+import lxml.html
+import json
+import string
+import sys
+import time
+import random
+import re
+import io
+sys.path.append('/home/hemaobin/workspace/stock')
+import mysqldb
+stock = mysqldb.StockDatabase()
+stock.connectdatabase()
+cursor = stock.getcursor()
 
 class Analyzer():
     def compare_price(self,symbol,price):
-        print('hello')
+        sql='select * from trade%s where volume > 1'%symbol
+        data = stock.select(cursor,sql)
+        if data == Null
 
--- getdata.py
+++ getdata.py
@@ -17,8 +17,6 @@ analyzer = analyzer.Analyzer()
 basic_url='http://hq.sinajs.cn/list='
 symbollist = ['sz000651','sz000333','sz300104','sz300415','sh601777','sz300051']
 code_list = ['000651','000333','300104','300415','601777','300051']
-print(len(code_list))
-print(code_list[0])
 headers = {'User-Agent':'gsi'}
 stock = mysqldb.StockDatabase()
 stock.connectdatabase()
