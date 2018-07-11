6a150b8632d4d1f844baaf0d347861952756ddd9
maobin <hemaobinz@163.com>
2018-07-11 12:08:20 +0800

    update

--- analyzer.py
+++ analyzer.py
@@ -12,25 +12,36 @@ import re
 import io
 sys.path.append('/home/hemaobin/workspace/stock')
 import mysqldb
-stock = mysqldb.StockDatabase()
-stock.connectdatabase()
-cursor = stock.getcursor()
 
 class Analyzer():
     def compare_price(self,symbol,price):
+        stock = mysqldb.StockDatabase()
+        stock.connectdatabase()
+        cursor = stock.getcursor()
+        sell_volume = 0
+        i=0
         sql = 'SELECT SUM(volume) AS volume FROM trade%s WHERE volume < 1' % symbol
-        sell_volume = stock.select(cursor,sql)
-        if sell_volume != None:
-            print(sell_volume[0][0])
+        volume = stock.select(cursor,sql)
+        if volume != None:
+            print(volume[0][0])
+            sell_volume = volume[0][0]
+            print(sell_volume)
 
         sql = 'SELECT @r:=@r+1 as rownum,a.* FROM trade%s a,(select @r:=0) b WHERE volume > 1 limit 10' % symbol
         data = stock.select(cursor,sql)
         if data != None:
             volume_list = []
-            i=0
             buy_volume=0
             for item in data:
                 buy_volume = buy_volume + item[2]
                 i = i+1
-                if buy_volume >= abs(100):
-                    print(buy_volume)
+                if buy_volume >= abs(sell_volume):
+                    print('buyvolume:',buy_volume,'num:',i)
+                    break
+        sql = 'SELECT * FROM trade%s WHERE volume > 1 limit %i,100' % (symbol,i-1)
+        data = stock.select(cursor,sql)
+        if data == None:
+            print('none')
+            return 0
+        for item in data:
+            print(item)
